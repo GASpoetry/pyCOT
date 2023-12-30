bacd1298fb15e72a67021cf68dd57fbc155b97e2
GH_GAS <142698953+GASpoetry@users.noreply.github.com>
2023-12-30 19:44:13 -0300

    Update setup.py
    GH suggestion test

--- pyCOT/setup.py
+++ pyCOT/setup.py
@@ -32,5 +32,6 @@ setup(
 
     # run-time dependencies that will be installed by pip
     # install_requires=['numpy','pandas','beautifulsoup4', 'bitarray','scipy','networkx','libroadrunner','lxml','pyvis','pypoman','matplotlib', 'joblib']
+    # GH: Below pypoman does not appear among the [].
     install_requires=['numpy','pandas','beautifulsoup4', 'bitarray','scipy','networkx','libroadrunner','lxml','pyvis','matplotlib','joblib','pulp','seaborn']
 )
